e0087559cff10442e2a67bb75e23f4f8642749f0
Sigurd Storve <sigurd.storve@ntnu.no>
2015-06-17 20:31:30 +0200

    Added radio buttons to select knot vector type [not yet functional]

--- src/widgets/KnotVectorWidget.cpp
+++ src/widgets/KnotVectorWidget.cpp
@@ -1,5 +1,7 @@
 #include <QVBoxLayout>
 #include <QTableWidget>
+#include <QGroupBox>
+#include <QRadioButton>
 #include "KnotVectorWidget.hpp"
 
 KnotVectorWidget::KnotVectorWidget(QWidget* parent, Qt::WindowFlags f)
@@ -9,10 +11,27 @@ KnotVectorWidget::KnotVectorWidget(QWidget* parent, Qt::WindowFlags f)
 
     m_table_widget = new QTableWidget(1, 10);
     v_layout->addWidget(m_table_widget);
-    
-    autogenerate_headers();
-    
+
+    auto rb_box = new QVBoxLayout;
+    auto group_box = new QGroupBox("Mutually exclusive choices");
+    auto rb_clamped = new QRadioButton("Clamped");
+    rb_clamped->setChecked(true);
+    auto rb_open    = new QRadioButton("Open");
+    rb_open->setChecked(false);
+    auto rb_closed  = new QRadioButton("Closed");
+    rb_closed->setChecked(false);
+    auto rb_custom  = new QRadioButton("Custom");
+    rb_custom->setChecked(false);
+    rb_box->addWidget(rb_clamped);
+    rb_box->addWidget(rb_open);
+    rb_box->addWidget(rb_closed);
+    rb_box->addWidget(rb_custom);
+    rb_box->addStretch(1);
+    group_box->setLayout(rb_box);
+    v_layout->addWidget(group_box);
+
     setLayout(v_layout);
+    autogenerate_headers();
 }
 
 void KnotVectorWidget::autogenerate_headers() {
